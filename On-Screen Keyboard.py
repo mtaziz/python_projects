887c18dc9d8f6ce8debfcc48cc3dd73a15c084b8
legitlearner <77491848+Robin-Yadav@users.noreply.github.com>
2021-04-03 22:24:40 +0530

    Update On-Screen Keyboard.py

--- On-Screen Keyboard.py
+++ On-Screen Keyboard.py
@@ -1,17 +1,22 @@
 from tkinter import *
 
+# Main window
 root = Tk()
 root.title('On-Screen Keyboard')
 root.configure(bg='black')
 
+# Disabling maximize
 root.resizable(False, False)
 
+# Generating area for typing text.
 text_area = Text(root, height=10, width=80, font=('arial', 10))
 text_area.grid(row=1, columnspan=40)
 
+# Initial point for rows and columns.
 rows = 2
 columns = 0
 
+# List of keys
 keys = ['\'', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'BackS', 'Del',
         'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '\\', '7', '8', '9',
         'caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'Enter', '4', '5', '6',
@@ -20,6 +25,7 @@ keys = ['\'', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'BackS
 
 
 def press(value):
+    '''This function contains actions for different buttons.'''
     if value == 'Space':
         text_area.insert(INSERT, ' ')
 
@@ -38,6 +44,7 @@ def press(value):
         text_area.insert(INSERT, screen_text[:-2])
 
     elif value == '↑ Shift':
+        # List of keys when shift is pressed.
         shift_up_keys = ['\"', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'BackS', 'Del',
                          'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '|', '7', '8', '9',
                          'caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', 'Enter', '4', '5', '6',
@@ -51,8 +58,7 @@ def press(value):
             command = lambda key=button: press(key)
 
             if button == 'Space':
-                Button(root, bg='light sky blue', width=95, text=button, command=command).grid(row=rows, column=columns,
-                                                                                               columnspan=15)
+                Button(root, bg='light sky blue', width=95, text=button, command=command).grid(row=rows, column=columns, columnspan=15)
             else:
                 Button(root, bg='light sky blue', width=5, text=button, command=command).grid(row=rows, column=columns)
                 columns += 1
@@ -62,6 +68,7 @@ def press(value):
                     rows += 1
 
     elif value == '↓ Shift':
+        # List of keys when shift is pressed again.
         shift_dn_keys = ['\'', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'BackS', 'Del',
                          'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '\\', '7', '8', '9',
                          'caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'Enter', '4', '5', '6',
@@ -75,8 +82,7 @@ def press(value):
             command = lambda key=button: press(key)
 
             if button == 'Space':
-                Button(root, bg='light sky blue', width=95, text=button, command=command).grid(row=rows, column=columns,
-                                                                                               columnspan=15)
+                Button(root, bg='light sky blue', width=95, text=button, command=command).grid(row=rows, column=columns, columnspan=15)
             else:
                 Button(root, bg='light sky blue', width=5, text=button, command=command).grid(row=rows, column=columns)
                 columns += 1
@@ -86,6 +92,7 @@ def press(value):
                     rows += 1
 
     elif value == 'caps':
+        # List of keys when caps is pressed.
         caps_keys = ['\'', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'BackS', 'Del',
                      'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '\\', '7', '8', '9',
                      'CAPS', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', 'Enter', '4', '5', '6',
@@ -99,8 +106,7 @@ def press(value):
             command = lambda key=button: press(key)
 
             if button == 'Space':
-                Button(root, bg='light sky blue', width=95, text=button, command=command).grid(row=rows, column=columns,
-                                                                                               columnspan=15)
+                Button(root, bg='light sky blue', width=95, text=button, command=command).grid(row=rows, column=columns, columnspan=15)
             else:
                 Button(root, bg='light sky blue', width=5, text=button, command=command).grid(row=rows, column=columns)
                 columns += 1
@@ -110,6 +116,7 @@ def press(value):
                     rows += 1
 
     elif value == 'CAPS':
+        # Using the global keys list when caps is pressed again.
         global keys
         rows = 2
         columns = 0
@@ -118,8 +125,7 @@ def press(value):
             command = lambda key=button: press(key)
 
             if button == 'Space':
-                Button(root, bg='light sky blue', width=95, text=button, command=command).grid(row=rows, column=columns,
-                                                                                               columnspan=15)
+                Button(root, bg='light sky blue', width=95, text=button, command=command).grid(row=rows, column=columns, columnspan=15)
             else:
                 Button(root, bg='light sky blue', width=5, text=button, command=command).grid(row=rows, column=columns)
                 columns += 1
@@ -131,15 +137,18 @@ def press(value):
     else:
         text_area.insert(INSERT, value)
 
+    # This will make sure that cursor is continuously blinking to make it visible to us.
     text_area.focus_set()
 
 
+# Looping through the list of keys and generating buttons in our root window.
 for button in keys:
+    # Command for our buttons.
     command = lambda key=button: press(key)
 
+    # Generating space button separately.
     if button == 'Space':
-        Button(root, bg='light sky blue', width=95, text=button, command=command).grid(row=rows, column=columns,
-                                                                                       columnspan=15)
+        Button(root, bg='light sky blue', width=95, text=button, command=command).grid(row=rows, column=columns, columnspan=15)
     else:
         Button(root, bg='light sky blue', width=5, text=button, command=command).grid(row=rows, column=columns)
         columns += 1
@@ -148,4 +157,5 @@ for button in keys:
             columns = 0
             rows += 1
 
+# Function call
 mainloop()
